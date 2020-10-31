4a68e6546eed7d063ab90294ae4acb01065a506e
sun <ehdghks789@naver.com>
2020-10-31 17:04:50 +0900

    전문가를 위한 머신러닝 솔루션 책 예제

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ for i in range(len(data.columns)):
     cleancolunms.append(data.columns[i].replace('-', '').lower())
 data.columns = cleancolunms
 print(data.head())
+data_col = data[data.columns[1:]]
+print(data_col.describe())
+print('median(중위수):\n', data_col.median(), '\n')
+print('mean(평균):', data_col.mean())
+print('colunms:', data.columns)
+total_len = len(data['seriousdlqin2yrs'])
+percentage_labels = (data['seriousdlqin2yrs'].value_counts()/total_len)*100
+print(percentage_labels)
+print(data['seriousdlqin2yrs'].value_counts())
+
